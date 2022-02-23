c06011f1bc587e4494e46f80ce2fed93564b57f8
Joao <jrodriigues@gmail.com>
2022-02-23 19:04:36 +0000

    tidying up

--- testing_first50primes.py
+++ testing_first50primes.py
@@ -1,8 +1,7 @@
-from numpy import number
 from functions import check_if_prime
 import csv
 
-with open('list_of_50_primes.csv') as f:
+with open(' s.csv') as f:
     reader = csv.reader(f)
     
     row = next(reader)
